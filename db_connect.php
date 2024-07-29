b94b23813e0c90adbec849594dc7a0c2257e5efb
Vince Lauron <lauronvincesimuelle@gmail.com>
2024-07-29 21:56:29 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','u510162695_fms_db_root','1Fms_db_root','u510162695_fms_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('127.0.0.1','u510162695_fms_db_root','1Fms_db_root','u510162695_fms_db')or die("Could not connect to mysql".mysqli_error($con));
