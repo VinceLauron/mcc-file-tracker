61cc0189f95f4f94c7d50699359fd320328a823b
Vince Lauron <lauronvincesimuelle@gmail.com>
2024-07-29 23:46:01 +0800

    Update login.php

--- applicant/login.php
+++ applicant/login.php
@@ -3,11 +3,6 @@ session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password']; // In this case, the password is the verification code
-   // Database connection
-    $conn = new mysqli("12.0.0.1", "u510162695_fms_db_root", "1Fms_db_root", "u510162695_fms_db");
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
 
 
     // Check if email and verification code match
