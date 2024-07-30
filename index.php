a94a08c6d47011abc712e784148007cac6ec8261
Vince Lauron <vince@example.com>
2024-07-30 16:06:01 +0800

    new

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
         <nav class="navbar navbar-expand-lg custom_nav-container ">
           <a class="navbar-brand" href="index.php">
             <span>
-              MCC DOCUMENT TRACKER
+              MCC DOCUMENT TRACKERs
             </span>
              
           </a>
