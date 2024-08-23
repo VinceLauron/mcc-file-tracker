14bb9987dfe7687879f09047875cf68a7e0fff57
Vince Lauron <vince@example.com>
2024-08-23 15:03:05 +0800

    commit

--- database/fms_db.sql
+++ database/fms_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 20, 2024 at 03:06 PM
+-- Generation Time: Jul 19, 2024 at 05:37 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -51,9 +51,7 @@ CREATE TABLE `applicant` (
 --
 
 INSERT INTO `applicant` (`id`, `verification_code`, `fullname`, `email`, `password`, `contact`, `address`, `sex`, `dob`, `occupation`, `id_number`, `year_graduated`, `school_graduated`, `program_graduated`, `admission`, `is_verified`) VALUES
-(88, '643654', 'Roxan Hilba', 'roxanhilba17@gmail.com', '', '09505501863', '', 'Female', '2000-06-28', '', '2020-1212', '2023', '', 'BSIT', '2020', 1),
-(89, '979722', 'vince lauron', 'lauronvince13@gmail.com', '', '09094513507', '', 'Male', '2002-12-30', '', '2021-1412', '2020-2021', '', 'BSIT', '2021', 1),
-(90, '662364', 'Bryan James Desuyo', 'bryanjamesdesuyo15@gmail.com', '', '09123767434', '', 'Male', '2003-05-15', '', '2021-1407', '2025', '', 'BSIT', '2021', 1);
+(88, '643654', 'Roxan Hilba', 'roxanhilba17@gmail.com', '', '09505501863', '', 'Female', '2000-06-28', '', '2020-1212', '2023', '', 'BSIT', '2020', 1);
 
 -- --------------------------------------------------------
 
@@ -157,17 +155,11 @@ INSERT INTO `notifications` (`id`, `user_email`, `message`, `status`, `date_crea
 (3, 'roxanhilba17@gmail.com', 'New request submitted by Vince Lauron', '1', '2024-07-17 08:29:04'),
 (4, 'roxanhilba17@gmail.com', 'New request submitted by vince lauron simuelle', '1', '2024-07-17 08:44:28'),
 (5, 'roxanhilba17@gmail.com', 'New request submitted by Vince Lauron', '1', '2024-07-17 08:50:02'),
-(6, 'roxanhilba17@gmail.com', 'New request submitted by Joebert Bilbao', '1', '2024-07-17 08:57:38'),
-(7, 'roxanhilba17@gmail.com', 'New request submitted by Vince Lauron', '1', '2024-07-17 09:08:51'),
-(8, 'roxanhilba17@gmail.com', 'New request submitted by vince lauron simuelle', '1', '2024-07-17 09:22:07'),
-(9, 'roxanhilba17@gmail.com', 'New request submitted by vincelauron', '1', '2024-07-19 03:34:15'),
-(10, 'roxanhilba17@gmail.com', 'New request submitted by vince lauron simuelle', '1', '2024-07-19 03:36:41'),
-(11, 'roxanhilba17@gmail.com', 'New request submitted by Joebert Bilbao', '1', '2024-07-19 03:43:05'),
-(12, 'lauronvince13@gmail.com', 'New request submitted by Vince Lauron', '1', '2024-07-19 11:12:47'),
-(13, 'roxanhilba17@gmail.com', 'New request submitted by Vince Lauron', '1', '2024-07-20 02:48:22'),
-(14, 'roxanhilba17@gmail.com', 'New request submitted by Vince Lauron', '1', '2024-07-20 06:03:30'),
-(15, 'bryanjamesdesuyo15@gmail.com', 'New request submitted by Bryan James Desuyo', 'unread', '2024-07-20 12:46:17'),
-(16, 'roxanhilba17@gmail.com', 'New request submitted by Vince Lauron', 'unread', '2024-07-20 13:05:05');
+(6, 'roxanhilba17@gmail.com', 'New request submitted by Joebert Bilbao', 'unread', '2024-07-17 08:57:38'),
+(7, 'roxanhilba17@gmail.com', 'New request submitted by Vince Lauron', 'unread', '2024-07-17 09:08:51'),
+(8, 'roxanhilba17@gmail.com', 'New request submitted by vince lauron simuelle', 'unread', '2024-07-17 09:22:07'),
+(9, 'roxanhilba17@gmail.com', 'New request submitted by vincelauron', 'unread', '2024-07-19 03:34:15'),
+(10, 'roxanhilba17@gmail.com', 'New request submitted by vince lauron simuelle', 'unread', '2024-07-19 03:36:41');
 
 -- --------------------------------------------------------
 
@@ -207,8 +199,9 @@ INSERT INTO `request` (`id`, `email`, `fullname`, `course`, `contact`, `id_numbe
 (216, 'jbbilbao80@gmail.com', 'Joebert Bilbao', 'BACHELOR OF SCIENCE IN SECONDARY EDUCATION MAJOR IN FILIPINO', '0945452582', '2021-1412', 'TRANSCRIPT OF RECORDS', 'for testing', '', '2024-07-16 12:25:00', 'rejected'),
 (217, 'jbbilbao80@gmail.com', 'rolly recabar', 'BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY', '31414141423', '2021-1602', 'TRANSCRIPT OF RECORDS', 'testing', '', '2024-07-16 12:26:00', 'rejected'),
 (218, 'jbbilbao80@gmail.com', 'Joebert Bilbao', 'BACHELOR OF SCIENCE IN BUSINESS ADMINISTRATION MAJOR IN FINANCIAL MANAGEMENT', '09866545643', '2021-1412', 'TRANSCRIPT OF RECORDS', 'hahaha', '', '2024-07-16 12:27:00', 'pending'),
-(236, 'lauronvince13@gmail.com', 'Vince Lauron', 'BACHELOR OF SCIENCE IN HOSPITALITY MANAGMENT', '09094513507', '2021-1412', 'TRANSCRIPT OF RECORDS', 'dfsdf', '', '2024-07-19 19:12:00', 'rejected'),
-(240, 'bryanjamesdesuyo15@gmail.com', 'Bryan James Desuyo', 'BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY', '0945452582', '2021-1407', 'TRANSCRIPT OF RECORDS', 'for emplyoment', 'pwede na makuha', '2024-07-20 20:46:00', 'released');
+(219, 'roxanhilba17@gmail.com', 'Roxan Hilba', 'BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY', '09866545643', '2021-1412', 'Good Moral Certificates', 'for work', '', '2024-07-16 13:47:00', 'released'),
+(232, 'roxanhilba17@gmail.com', 'vince lauron simuelle', 'BACHELOR OF SCIENCE IN SECONDARY EDUCATION MAJOR IN FILIPINO', '09866545643', '111111', 'TRANSCRIPT OF RECORDS', 'hahaha', '', '2024-07-17 17:22:00', 'onprocess'),
+(234, 'roxanhilba17@gmail.com', 'vince lauron simuelle', 'BACHELOR OF SCIENCE IN ELEMENTARY EDUCATION', '31414141423', '2021-1412', 'GOOD MORAL CERTIFICATES', 'a', '', '2024-07-19 11:36:00', 'pending');
 
 -- --------------------------------------------------------
 
@@ -221,18 +214,18 @@ CREATE TABLE `users` (
   `name` varchar(200) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(200) NOT NULL,
-  `verification_code` varchar(50) NOT NULL,
-  `is_verified` varchar(100) NOT NULL,
-  `type` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1=admin'
+  `code` varchar(50) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1+admin , 2 = users'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `username`, `password`, `verification_code`, `is_verified`, `type`) VALUES
-(83, 'Administrator', 'lauronvincesimuelle@gmail.com', '7b7bc2512ee1fedcd76bdc68926d4f7b', '', 'Verified', 1),
-(84, 'vince lauron', 'anton@gmail.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', '', 'Verified', 1);
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `code`, `status`, `type`) VALUES
+(1, 'Administrator', 'admin@admin.com', 'admin123', '', '', 1),
+(5, 'Vince Lauron', 'lauronvince13@gmail.com', 'vince123', '', '', 1);
 
 --
 -- Indexes for dumped tables
@@ -288,7 +281,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `applicant`
 --
 ALTER TABLE `applicant`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
 
 --
 -- AUTO_INCREMENT for table `files`
@@ -312,13 +305,13 @@ ALTER TABLE `folders`
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `request`
 --
 ALTER TABLE `request`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=242;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=235;
 
 --
 -- AUTO_INCREMENT for table `users`
